21e7561fb9a5b7aca0f9f975046b7db6199710f4
Simon <simon.correa@upb.edu.co>
2024-01-28 22:18:28 -0500

    Cambios en el CSS

--- clave/clave.js
+++ clave/clave.js
@@ -1,43 +1,59 @@
 window.onload = init;
 
-document.addEventListener("DOMContentLoaded", function() {
-  const keyboard = document.getElementById("keyboard");
-  const inputBox = document.getElementById("inputBox");
-  const keys = keyboard.getElementsByClassName("key");
+let data = [0,1,2,3,4,5,6,7,8,9];
 
-  function shuffleKeyValues() {
-    const values = Array.from({ length: 10 }, (_, index) => index.toString());
-    values.sort(() => Math.random() - 0.5);
+ const keyboard = document.getElementById("keyboard");
+ const inputBox = document.getElementById("inputBox");
+ const keys = keyboard.getElementsByClassName("key");
+const borrar = document.getElementById("borrar");
+const entrar = document.getElementById("entrar");
+
+function init() {
+  
+  keyboard.addEventListener("mouseenter", function() {
+    desordenarNumeros();
+    mostrarNumeros();
+  });
+
+  keyboard.addEventListener("mouseleave", function() {
+    ocultarNumeros();
+  });
+
+  Array.from(keys).forEach(key => {
+    key.addEventListener("click", function() {
+      inputBox.value += key.getAttribute("data-value"); 
+    });
+  });
+  
+  borrar.addEventListener("click", borrarNumero);
+  
+  entrar.addEventListener("click", () =>{
+    alert("Vous êtes bienvenu(e)");
+    inputBox.value = "";
+  });
+};
+
+function desordenarNumeros() {
+    //const numeros = Array.from({ length: 10 }, (_, index) => index.toString());
+    data.sort(() => Math.random() - 0.5);
 
     Array.from(keys).forEach((key, index) => {
-      key.setAttribute("data-value", values[index]);
+      key.setAttribute("data-value", data[index]);
     });
   }
 
-  function showNumbers() {
+  function mostrarNumeros() {
     Array.from(keys).forEach(key => {
       key.textContent = key.getAttribute("data-value");
     });
   }
 
-  function hideNumbers() {
+  function ocultarNumeros() {
     Array.from(keys).forEach(key => {
       key.textContent = "*";
     });
   }
 
-  keyboard.addEventListener("mouseenter", function() {
-    shuffleKeyValues();
-    showNumbers();
-  });
-
-  keyboard.addEventListener("mouseleave", function() {
-    hideNumbers();
-  });
-
-  Array.from(keys).forEach(key => {
-    key.addEventListener("click", function() {
-      inputBox.value += key.getAttribute("data-value");
-    });
-  });
-});
+  function borrarNumero(){
+    inputBox.value = inputBox.value.slice(0, -1);
+  }
